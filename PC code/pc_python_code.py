36849a1a8dae65c1e76b90a63ed98ab47572ec60
unknown <judenkereuwem@gmail.com>
2024-02-08 09:30:57 +0100

    first commit

---
+++ PC code/pc_python_code.py
@@ -0,0 +1,48 @@
+
+#Include needed libraries
+import os
+import pandas as pd
+import csv
+import serial
+import time
+from datetime import datetime
+
+
+#Serial comm config
+ser = serial.Serial('COM42', 115200)
+time.sleep(2)
+
+
+#Get current date and time for naming csv file
+now = datetime.now()
+d = datetime(1, 1, 1).now()
+dtString = d.strftime('{}_{}_{}'.format(d.hour%12,d.minute,d.second))
+dString = now.strftime('%d_%m_%Y')
+
+
+#Create new .csv file
+pathDir = 'file'
+filename = f'{dtString}.csv'
+df = pd.DataFrame() 
+df.to_csv(f"{pathDir}/{filename}", index=False, encoding='utf-8')
+
+
+
+
+#Open csv file and add headers
+with open(f"{pathDir}/{filename}",'r+') as f:
+    #f.writelines('Time,temp,light,motion')   #file header
+    
+    while True:
+        #Read incomming data from Pico
+        b = ser.readline().decode('ascii').strip()
+        print(b)
+
+        #Log incoming data from pico and current time to csv file
+        now = datetime.now()
+        d = datetime(1, 1, 1).now()
+        am_pm = 'am' if d.hour<12 else 'pm'
+        f.writelines(f'\n{b}')
+        f.flush()
+        
+    #ser.close() 
---
+++ PicoW code/main.py
@@ -0,0 +1,23 @@
+# Import necessary modules
+from machine import Pin
+import bluetooth
+from ble_simple_peripheral import BLESimplePeripheral
+
+# Create a Bluetooth Low Energy (BLE) object
+ble = bluetooth.BLE()
+
+# Create an instance of the BLESimplePeripheral class with the BLE object
+sp = BLESimplePeripheral(ble)
+
+# Create a Pin object for the onboard LED, configure it as an output
+led = Pin("LED", Pin.OUT)
+
+# Define a callback function to handle received data
+def on_rx(data):
+    print(data.decode('ascii').strip())
+
+# Start an infinite loop
+while True:
+    if sp.is_connected():  # Check if a BLE connection is established
+        sp.on_write(on_rx)  # Set the callback function for data reception
+
